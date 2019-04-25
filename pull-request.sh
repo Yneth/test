3be9360dee77bb52fe577d27f09584d0d6502e20
abondarenko <abondarenko@kaaiot.io>
2019-04-26 00:37:13 +0300

    setup

---
+++ pull-request.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
+
+make_pr() {
+    git checkout $TAVIS_BRANCH
+    diff=$(git diff HEAD~1 -- test)
+    [ -z "$diff" ] && echo "Test file is empty." || hub pull-request -m "test"
+}
+
+setup_git
+make_pr
