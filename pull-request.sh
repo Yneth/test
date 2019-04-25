822c05624528842ed5d8ad201fe116f1671fb8b8
Travis CI <travis@travis-ci.org>
2019-04-26 01:14:52 +0300

    test

--- pull-request.sh
+++ pull-request.sh
@@ -2,14 +2,12 @@
 
 build_head=""
 
-echo "$TRAVIS_BRANCH"
-
 setup_git_config() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
 }
 
-checkout_branch() {
+setup_git_branches() {
     # Keep track of where Travis put us.
     # We are on a detached head, and we need to be able to go back to it.
     build_head=$(git rev-parse HEAD)
@@ -21,16 +19,12 @@ checkout_branch() {
     git fetch
     # optionally, we can also fetch the tags
     git fetch --tags
-
-    # create the tacking branches
-    git checkout -qf $TRAVIS_BRANCH
 }
 
 make_pr() {
-    git checkout $TAVIS_BRANCH
-    git status
+    git checkout -qf $TAVIS_BRANCH
     diff=$(git diff HEAD~1 -- test)
-    [ -z "$diff" ] && echo "Test file is empty." || hub pull-request -m "test"
+    [ -z "$diff" ] && echo "Test file is empty." || hub pull-request -m "test [skip ci]"
 }
 
 checkout_build() {
@@ -38,6 +32,6 @@ checkout_build() {
 }
 
 setup_git_config
-checkout_branch
+setup_git_branches
 make_pr
 checkout_build
