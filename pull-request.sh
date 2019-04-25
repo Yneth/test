ed08e9b6bb53475408b751a532ee3be265f47221
Travis CI <travis@travis-ci.org>
2019-04-26 01:35:00 +0300

    test

--- pull-request.sh
+++ pull-request.sh
@@ -32,7 +32,7 @@ make_pr() {
     git status
     echo "$TRAVIS_BRANCH"
 
-    git checkout origin/$TAVIS_BRANCH
+    git checkout -f $TAVIS_BRANCH
     diff=$(git diff HEAD~1 -- test)
     [ -z "$diff" ] && echo "Test file is empty." || hub pull-request -m "test [skip ci]"
 }
